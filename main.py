c331c6754c8d22e923a9525c2ef55df4f7e72ecb
Christy Chan <christychanseewai@yahoo.com>
2022-01-30 07:14:12 +0000

    up to tsne but ingredient list not clean

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from typing import Dict, List, Union
+from zlib import DEF_BUF_SIZE
 import json_lines
+import numpy as np
 
 if __name__ == "__main__":
     print("Hello world!")
@@ -19,6 +21,50 @@ for item in products:
         option.append(item['category'])
 print(option)
 # filter data by given option
-def recommender(option):
+
+def recommender(opt):
+    df = []
+    for item in products:
+        if opt == item['category']:
+            df.append(item)
+    # print(df)
     
-# tokenize
+# creating a dictionary of ingredients, need to clean up ingredient (eg with *s, multiple names for water)
+    ingred_index = {}
+    igs = []
+    index = 0
+    i = 0
+    for item in df:
+        igs.append(item['ingredients'])
+        for i in range(len(item['ingredients'])):
+            if item['ingredients'][i] not in ingred_index:
+                ingred_index[index] = item['ingredients'][i]
+            index +=1
+    # print(ingred_index)
+# mapping encodings to items
+    X = len(df)
+    Y = len(ingred_index)
+
+    big_matrix = np.zeros((X,Y))
+
+    def encoder(ing):
+        a = np.zeros(Y)
+        for i in ing:
+            #get the index for each ingredient
+            # index = ingred_index[i]
+            index = list(ingred_index.keys())[list(ingred_index.values()).index(i)]
+            # print(index)
+            a[index] = 1
+        return a 
+    
+    i = 0
+    print(len(igs))
+    for ingreds in igs:
+        big_matrix[i, :] = encoder(ingreds)
+        i += 1
+
+
+
+#can add the multiple products later
+
+print(recommender('MOISTURISER'))
